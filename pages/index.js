5e1153ba3244de3809fbd6dc93e7393c0598c3f7
gmiesner <ella.miesner@gmail.com>
2021-02-17 19:31:02 -0600

    test test

--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,7 @@ const Index = () => {
 <img src = "https://brand.mst.edu/media/universityadvancement/brand/logos/athletics/mascotmarks/JOE_MINER_FC.png" height = "75rem" alt = "s&t joe miner"/>
 <img src = "https://www.clipartmax.com/png/full/150-1509428_university-of-missouri-mizzou-tigers.png" height = "75rem" alt = "miz tiger"/>
 <img src = "https://upload.wikimedia.org/wikipedia/en/5/5c/UMSL_Tritons_logo.svg" height = "75rem" alt = "umsl triton"/>
-<img src = "https://dbukjj6eu5tsf.cloudfront.net/umkckangaroos.com/images/logos/site/site.png" height = "75rem" alt = "umkc roos"/>
+<img src = "site.png" height = "75rem" alt = "umkc roos"/>
       <header>
         <Search addClass={addClass} />
       </header>
