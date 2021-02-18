65d80a691d9530319fbe6975d56b7356c65b775b
gmiesner <ella.miesner@gmail.com>
2021-02-18 10:44:34 -0600

    o's r done - miz

--- data/scrap.js
+++ data/scrap.js
@@ -11,7 +11,7 @@ const browser = await puppeteer.launch();
 			let stri = document.querySelector('#courseinventorycontainer').innerText.split("\n");
 
 			let n = [];
-			for (i in stri){ if (stri[i].startsWith('N')){ n.push(stri[i]) }}
+			for (i in stri){ if (stri[i].startsWith('O')){ n.push(stri[i]) }}
 
 
 
@@ -45,4 +45,4 @@ browser.close()
 
 }
 
-scrape("http://catalog.missouri.edu/courseofferings/nep/")
+scrape("http://catalog.missouri.edu/courseofferings/ophth/")
